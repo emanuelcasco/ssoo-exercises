3d473b9c377fac354a262f467e32d470dcc8101f
Emanuel Casco <cascoemanuel@gmail.com>
2018-04-18 12:17:44 -0300

    Fix exercise 1

--- exercise-01/app.c
+++ exercise-01/app.c
@@ -1,9 +1,5 @@
 #include "app.h"
 
-char* string_concat (const char*, const char*);
-void string_concat_dinamyc(const char*, const char*, char**);
-void mail_split(const char* mail, char** user, char** dominio);
-
 int main(void) {
 // Punto a
   char* nombre = "Ema";
--- exercise-01/app.h
+++ exercise-01/app.h
@@ -7,6 +7,8 @@
 
 char* string_concat (const char*, const char*);
 void string_concat_dinamyc(const char*, const char*, char**);
-void mail_split(const char* mail, char** user, char** dominio);
+void splitAfter(const char*, char, char**);
+void splitBefore(const char*, char, char**);
+void mail_split(const char*, char**, char**);
 
 #endif /* EJERCICIO1_H_ */
